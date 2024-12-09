c76cbc40781c189d93d134f424b4c22d788f53cf
Aldric Adams <aldricadams12@gmail.com>
2024-12-09 14:35:39 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
   {
@@ -16,13 +17,22 @@ export const routes: Routes = [
     component: UserTasksComponent,
     children: [ 
       {
+        path: '',
+        redirectTo: 'tasks', // Voegt /tasks toe aand bijv. /users/u3 
+        pathMatch: 'prefix'
+      },
+      {
         path: 'tasks', // /users/<uid>/tasks
         component: TasksComponent
       },
       {
-        path: 'tasks/new',
+        path: 'tasks/new', // /users/<uid>/tasks/new
         component: NewTaskComponent
       }
     ]
   },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
