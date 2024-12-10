ee72b2d6a92dc0bd5808106c2f754ba00838e787
Aldric Adams <aldricadams12@gmail.com>
2024-12-10 10:49:58 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
+
+// Toegang bepalen met Route Guard
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
 
 export const routes: Routes = [
   {
@@ -29,6 +40,8 @@ export const routes: Routes = [
         component: NewTaskComponent
       }
     ],
+    // Route Guard:
+    canMatch: [dummyCanMatch],
     // Statische data
     data: {
       message: 'Hello!'
